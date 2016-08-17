a7dbd660594cc8020adc69378266e4aadcd1b131
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-08-17 13:10:14 +0700

    first :rocket:

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # o2pdf
 API to convert office files to PDF directly
+
+## How to
+
+```` php
+    use O2Pdf\OfficeToPdf;
+    
+    $office = new OfficeToPdf('absolute/url/to/office/file/online');
+    $pdfUrl = $office->getPdfUrl();
+    var_dump($pdfUrl);
+````
+
---
+++ src/OfficeToPdf.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace O2Pdf;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\TransferException;
+
+class OfficeToPdf
+{
+    protected static $endpoint = 'https://docs.google.com/viewer';
+    protected $url;
+    protected $pdf;
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    public function getPdfUrl()
+    {
+        $client = new Client();
+        $query = [
+            'url' => $this->url,
+        ];
+        try {
+            $response = $client->request('GET', static::$endpoint, compact('query'));
+            if ($response->getStatusCode() == '200') {
+                $body = $response->getBody();
+                $content = $body->getContents();
+                $results = collect(explode(',', $content))->filter(function ($item) {
+                    return strpos($item, '\u003d"');
+                })->first();
+                $url = str_replace(['\u003d"', '"'], '', $results).'=?download=true';
+                return $url;
+            }
+        } catch (TransferException $e) {
+          //
+        }
+    }
+}
